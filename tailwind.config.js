13f50658151f1243fca059e8d5263e5ea6247f90
Sebastian1996x <73837995+Sebastian1996x@users.noreply.github.com>
2020-11-09 13:57:51 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,29 @@
 module.exports = {
+  theme: {
+    extend: {
+      color: {
+        primary: '#004f9f'
+        danger:  {
+          '50' : '#e4032e',
+        }
+        success: {
+          '50' : '#00983a',
+        }
+        hellerGrauton {
+          '50' : '#a4cedf',
+        }
+        dunklerGrauton {
+          '50' : '#454545',
+       }
+       
+    body: #000000;
+    H: #000000;
+    a: #3c3c3c;
+    background-image: radial-gradient(white, #dddddd);
+
+      }
+    }
+  }
   variants: {
     backgroundColor: ['responsive', 'hover', 'focus'],
     borderColor: ['responsive', 'hover', 'focus'],
